171b90554ef33d0e086f0061faf704df4e45ba6f
Braden Townsell <bradenjparrish@gmail.com>
2024-10-08 16:52:24 -0500

    Delete CrossDropCalc.Api directory
    no need for API, overly complex

--- CrossDropCalc.Api/Services/Interfaces/ICrossDropService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CrossDropCalc.Api.Services.Interfaces
-{
-    public interface ICrossDropService
-    {
-        string GetBlendedOctane(int beforeGallons, double beforeOctane, int afterCrossDropGallons, double crossDropOctane, int tankCapacity, double octaneThreshold);
-    }
-}
